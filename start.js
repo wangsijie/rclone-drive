d347961d09e83388195b4bc81d6259d232540b28
WangSijie <i@sijie.wang>
2019-04-12 11:25:04 +0800

    Add eslint\prettier

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, () => {
     console.log(`RClone-Drive running on http://localhost:${port}`);
 });
